42067ffb5445112239a5ce9ba80314b59ca6adf3
Zhongzhi Hu <zhongzhihu@hotmail.com>
2026-01-25 15:54:37 +0100

    Initial Commit

---
+++ kawaii2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kawaii2
+//
+//  Created by Zhongzhi on 25.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kawaii2/kawaii2App.swift
@@ -0,0 +1,17 @@
+//
+//  kawaii2App.swift
+//  kawaii2
+//
+//  Created by Zhongzhi on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct kawaii2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
